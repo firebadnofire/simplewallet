12cc2e15c1bd1f64049bb2bca24ca28e4c58253e
firebadnofire <william@archuser.org>
2026-06-14 22:26:33 -0400

    Fix release workflow defaults and bump v1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "org.archuser.wallet"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
